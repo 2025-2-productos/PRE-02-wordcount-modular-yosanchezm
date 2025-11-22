08268d47719a87fca0d09759c8a090985d636817
Camilo Sánchez <yosanchezm@unal.edu.co>
2025-11-22 11:06:00 -0500

    Add entry point for wordcount module and update main function to accept command line arguments

---
+++ homework/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for the homework.wordcount module."""
+
+from .src.wordcount import main
+
+if __name__ == "__main__":
+    main()
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -9,13 +9,13 @@ from ._internals.write_word_counts import write_word_counts
 
 
 def main():
-    """
+    
     if len(sys.argv) != 3:
         print("Usage: python -m homework <input_folder> <output_folder>")
         sys.exit(1)
-    """
-    input_folder = "data/input"
-    output_folder = "data/output"
+    
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
 
     all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
